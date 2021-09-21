2751d1745feaa665de5f4c64a6cd1a6fbf3238bb
moween <moween1998@gmail.com>
2021-09-21 10:41:54 +0100

    Change MUI default breakpoints

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import Header from './components/Header';
+import { createTheme, ThemeProvider } from '@material-ui/core';
 import './css/App.css';
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 375,
+      md: 768,
+      lg: 1024,
+      xl: 1140,
+    },
+  }
+})
+
 function App() {
-  return null;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header />
+      </div>
+    </ThemeProvider>    
+  );
 }
 
 export default App;
