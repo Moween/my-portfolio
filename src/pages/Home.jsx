68f4073e8e179bb5fac2f0bf6fc6818c04ca9fa3
moween <moween1998@gmail.com>
2021-09-22 17:33:16 +0100

    Add and style social-icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import bgImg from '../images/Avater@2x.png'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { makeStyles } from '@material-ui/styles';
 
 
@@ -43,7 +46,7 @@ const Home = () => {
     <main className="main">
       <div className="greeting-text">
         <Typography variant="h1" component="p" style={{color: '#cb6036'}} className={greetingText}>
-        Front-end developer
+          Front-end developer
         </Typography>
       </div>
       <div className="intro-text">
@@ -55,6 +58,11 @@ const Home = () => {
       </div>
       <div>
         <img className={img} src={bgImg} alt="laptop-guy" />
+      </div>
+      <div className="social-icons-container">
+        <TwitterIcon className="social-icon"/>
+        <LinkedInIcon className="social-icon"/>
+        <GitHubIcon className="social-icon"/>
       </div>
       <Button 
         variant="contained"
