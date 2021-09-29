b9170f95b65b6fd23620c75b0cc911a37f15d8a5
moween <moween1998@gmail.com>
2021-09-29 21:57:56 +0100

    Create projects data

---
+++ src/projectsData.js
@@ -0,0 +1,53 @@
+const projects = [
+  {
+    id: 'pj-01',
+    title: 'Weather-App',
+    projectOverview: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+    perspiciatis a sunt placeat neque quaerat similique ex voluptatum
+    deleniti, officia iste beatae ab, architecto non, pariatur
+    laudantium sed magnam quo.`,
+    preview: 'https://orbie-weather-app.herokuapp.com/'
+  },
+  
+  {
+    id: 'pj-02',
+    title: 'Todo-App',
+    projectOverview: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+    perspiciatis a sunt placeat neque quaerat similique ex voluptatum
+    deleniti, officia iste beatae ab, architecto non, pariatur
+    laudantium sed magnam quo.`,
+    preview: 'https://moween.github.io/active-todo/',
+  },
+
+  {
+    id: 'pj-03',
+    title: 'Rock-Paper-Scissors',
+    projectOverview: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+    perspiciatis a sunt placeat neque quaerat similique ex voluptatum
+    deleniti, officia iste beatae ab, architecto non, pariatur
+    laudantium sed magnam quo.`,
+    preview: 'https://moween.github.io/guess-game/'
+  },
+
+  {
+    id: 'pj-04',
+    title: 'Mo-Fashion-Store',
+    projectOverview: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+    perspiciatis a sunt placeat neque quaerat similique ex voluptatum
+    deleniti, officia iste beatae ab, architecto non, pariatur
+    laudantium sed magnam quo.`,
+    preview: 'https://moween.github.io/mo-fashion-store/'
+  },
+
+  {
+    id: 'pj-05',
+    title: 'Etch-a-sketch',
+    projectOverview: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+    perspiciatis a sunt placeat neque quaerat similique ex voluptatum
+    deleniti, officia iste beatae ab, architecto non, pariatur
+    laudantium sed magnam quo.`,
+    preview: 'https://moween.github.io/etch-a-sketch'
+  }
+];
+
+export default projects;
