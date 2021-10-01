4fb85c600adef20a417b37db825b8a8e3d3ed4a6
moween <moween1998@gmail.com>
2021-10-01 23:13:42 +0100

    Style button component

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/styles';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
+  btn: {
+    padding: '0.43rem',
+    // justifyContent: 'space-between',
+    textTransform: 'capitalize',
+    width: (props) => {
+      if (props.colorType === 'yellow') {
+        return '180px';
+      }
+    },
+    backgroundColor: (props) => {
+      if (props.colorType === 'yellow') {
+        return '#f7f124';
+      }
+    },
+    transition: (props) => {
+      if (props.colorType === 'yellow') {
+        return 'backgroundColor 2s ease 1s';
+      }
+      return null;
+    },
+    transitionTimingFunction: 'ease',
+    transitionDelay: '1s',
+    '&:hover': {
+      backgroundColor: '#000',
+      color: '#fff',
+      border: '1px solid #f7f124',
+    },
+  },
   
-}))
+});
+
 
 const MyButton = (props) => {
-  const { value, href } = props;
+  const { btn } = useStyles(props);
+  const { value, href, endIcon } = props;
   return (
     <Button
       variant="contained" 
-      color="primary"
+      // color="primary"
+      className={btn}
       size="small" 
-      endIcon={<OpenInNewIcon />}
+      endIcon={endIcon}
       href={href}
       target="new"
     >
