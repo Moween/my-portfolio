7dec5ffcab6b6acc8cbfc5fbbd834d0f6c0a08db
moween <moween1998@gmail.com>
2021-10-01 23:12:56 +0100

    Update product-section styles

--- src/components/projectsComponent/Project.jsx
+++ src/components/projectsComponent/Project.jsx
@@ -1,18 +1,70 @@
 import React from 'react';
 import MyButton from '../MyButton';
 import Typography from '@material-ui/core/Typography';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew'
+
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
   section: {
-    height: '100vh',
+    height: '70vh',
+    width: (props) => {
+      if(props.index % 2) {
+        return '100vw';
+      }else {
+        return null;
+      }
+    },
+    marginLeft: (props) => {
+      if(props.index % 2) {
+        return 'calc(50% - 50vw)';
+      }else {
+        return null;
+      }
+    },
+    backgroundColor: (props) => {
+      if(props.index % 2) {
+        return '#212121';
+      }else {
+        return null;
+      }
+    },
+    marginBottom: '2rem',
   },
   projectInfoCont: {
-    display: "flex",
+    display: 'flex',
     alignItems: 'center',
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
+    flexDirection : (props) => {
+      if(props.index % 2) {
+        return 'row-reverse';
+      }else {
+        return 'row';
+      }
+    },
+    width: (props) => {
+      if(props.index % 2) {
+        return '84.5vw';
+      }else {
+        return null;
+      }
+    },
+    marginLeft: (props) => {
+      if(props.index % 2) {
+        return 'auto';
+      }else {
+        return null;
+      }
+    },
+    marginRight: (props) => {
+      if(props.index % 2) {
+        return 'auto';
+      }else {
+        return null;
+      }
+    },
     margintop: 20,
-    paddingTop: 30,
+    padding: 30,
     textAlign: 'left'
   },
   projectDetails: {
@@ -23,19 +75,21 @@ const useStyles = makeStyles((theme) => ({
   },
 
   imgBox: {
-    boxShadow: '0 3px 10px rgb(50, 50, 50)',
+    boxShadow: '0 2px 3px rgb(50, 50, 50)',
     width: '45%',
+    borderRadius: '5px'
   },
   screenShot: {
     width: '100%',
-    height: "350px",
+    height: '350px',
   },
 }));
 
 
-const Project = ({ projectData }) => {
-  const { imgUrl, projectOverview, title, preview } = projectData;
-  const {  section, projectInfoCont, projectDetails, screenShot, projectInfo, imgBox } = useStyles();
+const Project = (props) => {
+  console.log(props.index)
+  const { imgUrl, projectOverview, title, preview } = props.projectData;
+  const {  section, projectInfoCont, projectDetails, screenShot, projectInfo, imgBox } = useStyles(props);
   
   return (
     <section className={section}>
@@ -49,7 +103,12 @@ const Project = ({ projectData }) => {
               <Typography variant="body1" component="p" className={projectInfo}>
                 {projectOverview}
               </Typography>
-              <MyButton value="Go to site" href={preview} />
+              <MyButton
+                value="Live Preview"
+                endIcon={<OpenInNewIcon />}
+                href={preview}
+                colorType="yellow"
+              />
             </div>
           </div>
       </article>
