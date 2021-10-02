709aaba32445384821652445f21042724e6f5b1d
moween <moween1998@gmail.com>
2021-10-02 21:42:16 +0100

    Update articles array.

--- src/data.js
+++ src/data.js
@@ -85,21 +85,16 @@ export const articles = [
     id: 'art01',
     title: 'JavaScript Loop Statement',
     intro: `When you hear loops, what image comes to your mind? 
-      yeah, you! The popular loops cereal was the first for me! 
-      As programmers, we often need to check or do something a certain number of times...`,
-    coverImg: `https://lh3.googleusercontent.com/2GmXds6gYWrW0MIj8MLMuglCn4
-    MR7QtLQW_k9syvxHkTOttu66-ub1_Bv0Zm1F8Xeu6M=s162`,
+      yeah, you! The popular loops cereal was the first for...`,
+    coverImg: '',
     previewLink: 'https://orbie.hashnode.dev/javascript-loop-statement',
   },
   {
     id: 'art02',
     title: 'Understanding JavaScript Variable Scope',
     intro: `If you are reading this article, you are reading my first article.
-      As a newbie in JavaScript, the fundamental word I heard a lot was variables.
-     It was mentioned in every book, article, and tutorial on JavaScript I came across...`,
-    coverImg: `https://orbie.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.
-    com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1614467705364%2FmJkMCBG8d.jpeg%3Fw%3D1600%26h%3D840%26
-    fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75`,
+      As a newbie in JavaScript, the fundamental word I heard a lot was...`,
+    coverImg: '',
     previewLink: 'https://orbie.hashnode.dev/understanding-javascript-variable-scope'
   }
 ];
