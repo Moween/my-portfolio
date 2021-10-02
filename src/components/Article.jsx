bfca4e077fca27b4a52d9001a38681309f9f1fcc
moween <moween1998@gmail.com>
2021-10-02 15:37:19 +0100

    Import HeadingText in the articles component.

---
+++ src/components/Article.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeadingText from './HeadingText';
+
+const Article = () => {
+  return (
+    <section>
+      <article>
+        <HeadingText text="Articles" />
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </article>
+    </section>
+  )
+}
+
+export default Article;
