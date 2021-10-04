22fbbfeb4ebf9c912d68c9821ff7eae6d9fb3336
moween <moween1998@gmail.com>
2021-10-04 07:32:39 +0100

    Add classnames

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -5,31 +5,36 @@ import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(() => ({
+  certList: {
+    marginTop: '4rem',
+  },
   list: {
     width: '70%',
     margin: 'auto',
   },
+  listItem:  {
+    borderBottom:  '1px solid #fff', 
+  },
   listText: {
     padding: '1rem',
-    // borderBottom: '1px solid #fff',
     color: '#ffffff',
     '&:hover': {
-      animation: 'bounce 0.5s alternate 1s',
-      
+      animation: 'bounce .3s ease 0s alternate',      
     },
   },
 }))
 
 const Achievement = () => {
-  const { list, listText } = useStyles();
+  const { list, listItem, listText, certList } = useStyles();
   return (
     <section className="achievement" id="achievement">
       <article>
         <HeadingText text="Trainings and Certifications" />
-        <div>
+        <div className={certList}>
           <ul className={list}>
+            <hr />
             {certifications.map((certificate, index) => (
-              <li key={index}>
+              <li key={index} className={listItem}>
                 <Typography variant="body1" 
                   component="p" 
                   className={listText}
