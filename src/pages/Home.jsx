436bc1c364ae2e62daf6f4c78cff4df8ff8be7f9
moween <moween1998@gmail.com>
2021-10-09 04:57:32 +0100

    Import styles from styes folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined'
 import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import { makeStyles } from '@material-ui/styles';
+
+import { useStyles } from '../utils/styles';
 import About from '../components/About';
 import Achievement from '../components/Achievement';
 import Article from '../components/Article';
@@ -14,36 +15,8 @@ import Contacts from '../components/Contacts';
 import MyProjects from '../components/projectsComponent/MyProjects';
 
 
-const useStyles = makeStyles((theme) => ({
-  greetingText: {
-    [theme.typography.h1.fontSize]: {
-      fontSize: '20rem',
-      lineHeight: '1.5rem',
-    },
-    paddingTop: '0.625rem',
-    opacity:  0.2,
-    wordSpacing: 20,
-    letterSpacing: '7px',
-    [theme.breakpoints.down('md')]: {
-      display: 'none',
-    }
-  },
-
-  introText: {
-    [theme.breakpoints.down('md')]: {
-      right: '10%',
-    }
-  },
-
-  img: {
-    [theme.breakpoints.down('sm')]: {
-      
-    }
-  }
-}));
-
 const Home = () => {
-  const { greetingText, img } = useStyles();
+  const { greetingText, img } = useStyles;
   return ( 
     <main>
       <section className="landing-section">
