98c9c3823172f59b6aa9b374ac9f6192899e2907
moween <moween1998@gmail.com>
2021-11-21 14:40:53 +0100

    Refactor social icons to SocialIcons component

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const SocialIcons = () => {
+  return (
+    <div className="social-icons-container">
+      <Button
+        className="social-icon"
+        href="https://www.twitter.com/iam_Moween"
+        target="new"
+      >
+        <TwitterIcon />
+      </Button>
+      <Button
+        className="social-icon"
+        href="https://www.linkedin.com/in/obiageli-ezeilo/"
+        target="new"
+      >
+        <LinkedInIcon className="social-icon" />
+      </Button>
+      <Button
+        className="social-icon"
+        href="https://www.github.com/Moween"
+        target="new"
+      >
+        <GitHubIcon className="social-icon" />
+      </Button>
+    </div>
+  );
+};
+
+export default SocialIcons;
