b9ffb96ee871c68cbc23ba712df18559365404ae
moween <moween1998@gmail.com>
2021-11-21 14:42:00 +0100

    Prettify code and re-position elements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,26 @@
-import React from "react";
-import { useTheme } from "@material-ui/styles"; // For mediaquery
-import Typography from "@material-ui/core/Typography";
-import img from "../images/personal_img.jpeg";
-import HeadingText from "./HeadingText";
-import { useStyles } from "../utils/styles";
+import React from 'react';
+import { useTheme } from '@material-ui/styles'; // For mediaquery
+import Typography from '@material-ui/core/Typography';
+import img from '../images/personal_img.jpeg';
+import HeadingText from './HeadingText';
+import { useStyles } from '../utils/styles';
 
 const About = () => {
   const theme = useTheme();
   const { avatarContainer, avatarImg, bioInfo, bio, aboutImgBox, personalImg } =
     useStyles(theme);
-  const avatar = img;
   return (
     <section id="about" className="about-section">
       <article>
         <HeadingText text="About me" />
         <div className={bioInfo}>
           <Typography className={bio} variant="body1">
-            <div className={avatarContainer}>
-              <img src={avatar} className={avatarImg} alt="Maureen" />
-            </div>
-            I am a self-taught
-            Front-end web developer, and an enthusiatic JavaScript lover,
-            a problem-solver, and  a team player with a
-            strong desire to learn.
-            My curiosity about how the web works
-            made my transition into the tech industry. Adept at using HTML, CSS, JavaScript,
-            ReactJS, Git, and modern front-end web tools to produce clean code.
-            When I am not coding, you'll find me outdoors or 
+            I am a self-taught Front-end web developer, and an enthusiatic
+            JavaScript lover, a problem-solver, and a team player with a strong
+            desire to learn. My curiosity about how the web works made my
+            transition into the tech industry. Adept at using HTML, CSS,
+            JavaScript, ReactJS, Git, and modern front-end web tools to produce
+            clean code. When I am not coding, you'll find me outdoors or
             catching up with my favorite tv shows.
           </Typography>
           <div className={aboutImgBox}>
