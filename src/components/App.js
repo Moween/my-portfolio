b01fe66e3246af1754810c877a0d0b6f9c6465ec
moween <moween1998@gmail.com>
2021-12-07 08:27:04 +0100

    Update Code:
    - Use mediaquery and useContext hook.
    - Pass in mediaQuery to context.
    - Prettify code.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
+import React, { createContext } from 'react';
 import { Route } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import { ThemeProvider } from '@material-ui/core';
+import { useMediaQuery } from '@material-ui/core';
+
 import Home from '../pages/Home';
 import Header from './Header';
 import Footer from './Footer';
-
 import { theme } from '../utils/styles';
 import '../css/App.css';
 
+export const MediaQueryContext = createContext();
+
 function App() {
+  const  mobile = useMediaQuery(theme.breakpoints.down(767));
+  const tablet = useMediaQuery(theme.breakpoints.between('md', 1023));
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth="xl">
-        <Header />
-        <Route path='/' component={Home} />
-        <Footer />
-      </Container>    
-    </ThemeProvider>
+    <MediaQueryContext.Provider value={{mobile, tablet}}>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="xl">
+          <Header />
+          <Route path="/" component={Home} />
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    </MediaQueryContext.Provider>
   );
 } 
 
