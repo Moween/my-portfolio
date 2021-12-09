ab2b0a81fbacc8be2329f2a8a9a646fee898a7b3
moween <moween1998@gmail.com>
2021-12-09 23:03:38 +0100

    Update
    - Import react-toastify package.
    -  Update mediaQuery breakpoints.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import { ThemeProvider } from '@material-ui/core';
 import { useMediaQuery } from '@material-ui/core';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Home from '../pages/Home';
 import Header from './Header';
@@ -13,8 +14,8 @@ import '../css/App.css';
 export const MediaQueryContext = createContext();
 
 function App() {
-  const  mobile = useMediaQuery(theme.breakpoints.down(767));
-  const tablet = useMediaQuery(theme.breakpoints.between('md', 1023));
+  const  mobile = useMediaQuery(theme.breakpoints.down(600));
+  const tablet = useMediaQuery(theme.breakpoints.between('600', 1023));
 
   return (
     <MediaQueryContext.Provider value={{mobile, tablet}}>
